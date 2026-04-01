2926411f3192d5aa9ebf5f59650cf862821ba2df
Yigit ARISOY <yigit@Yigits-Macbook-Pro.local>
2026-04-02 00:37:24 +1100

    updated

--- photos.js
+++ photos.js
@@ -19,6 +19,4 @@ const photos = [
   { file: "photo16.jpg", horizontal: true },
   { file: "photo17.jpg", horizontal: true  },
   { file: "photo18.jpg", horizontal: true },
-  { file: "photo19.jpg", horizontal: true },
-  { file: "photo20.jpg", horizontal: false },
 ];
